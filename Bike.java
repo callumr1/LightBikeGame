505e076d49a2f9e5fa16a5002d5aa5ab726e8fec
Callum Robertson <callumr1@hotmail.com>
2017-09-11 13:28:51 +1000

    Bike and Grid work
    Added functions and properties for the Bike and Grid classes

--- Bike.java
+++ Bike.java
@@ -1,2 +1,24 @@
 public class Bike {
+
+    private String bikeColour;
+    private int bikeID;
+    private int bikeSpeed;
+
+    public int getBikeID() {
+        return bikeID;
+    }
+
+    public void setBikeID(int bikeID) {
+        //sets the bikeID equal to the playerID
+        this.bikeID = bikeID;
+    }
+
+    public String getBikeColour() {
+        return bikeColour;
+    }
+
+    public void setBikeColour(String bikeColour) {
+        //sets the bikeColour equal to the playerColour
+        this.bikeColour = bikeColour;
+    }
 }
--- Grid.java
+++ Grid.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+
 public class Grid {
+
+    private int width;
+    private int height;
+
+    public Grid(){
+        width = 20;
+        height = 20;
+    }
+
+    public void setWidth(int width) {
+        //Allows the user to change the width of the grid
+        Scanner w = new Scanner(System.in);
+        System.out.print("Enter your player name >>> ");
+        this.width = w.nextInt();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(int height) {
+        //Allows the user to change the height of the grid
+        Scanner h = new Scanner(System.in);
+        System.out.print("Enter your player name >>> ");
+        this.height = h.nextInt();
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void displayGrid(int width, int height){
+        //displays the GUI for the grid
+    }
 }
